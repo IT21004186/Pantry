9f8eb53c2f079551f1386b25da32f49126291cc4
Harindu Nimshan <it21004186@my.sliit.lk>
2023-05-24 22:13:27 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 
 export const verifyToken = async (req, res, next) => {
     try{
-        
+               
+    } catch (err) {
+        res.status(500).json({ error: err.message })
     }
 }
