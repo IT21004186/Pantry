445fb55bd15c1122884f288899ba6766e5273019
Harindu Nimshan <it21004186@my.sliit.lk>
2023-06-25 09:52:02 +0530

    update index.js file

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -41,3 +41,6 @@ export const authSlice = createSlice({
         }
     }
 })
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost} = authSlice.actions;
+export default authSlice.reducer;
